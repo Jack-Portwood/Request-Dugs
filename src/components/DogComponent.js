98d8e2a322d07664941429c9acf2dcf746bf17f4
Jack Portwood <Jack.Portwood14@gmail.com>
2020-11-03 14:42:36 +0000

    can fetch object

---
+++ src/components/DogComponent.js
@@ -0,0 +1,34 @@
+import React,{useState,useEffect} from 'react'
+const DogComponent = () => {
+
+    const [hasError, setErrors] = useState(false)
+    const [dogs,setDogs] = useState({})
+
+    async function fetchData(){
+        const res = await fetch("https://dog.ceo/api/breeds/image/random");
+
+        res 
+            .json()
+            .then(res=> setDogs(res))
+            .catch(err=> setErrors(err));
+    }
+
+    useEffect(() =>{
+        fetchData();
+    },[])
+
+
+
+
+return (
+  <div>
+    <h1>Woof WOof</h1>
+    <span>{JSON.stringify(dogs)}</span>
+    <hr/>
+    <span> has error: {JSON.stringify(hasError)}</span>
+  </div>
+);
+}
+
+export default DogComponent;
+
